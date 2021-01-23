2abd2a8f7805aaf10f0f255a485493ec642a28b0
alireza <ali.hamidi@jointscopes.com>
2021-01-23 11:23:31 +0330

    fleet Module

--- projects/uae-iserve/src/app/core/general-services/window-resize.service.ts
+++ projects/uae-iserve/src/app/core/general-services/window-resize.service.ts
@@ -4,7 +4,7 @@ import { Subject } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class WindowResizeService {
     private onResize = new Subject();
-    public resize$ = this.onResize.asObservable();
+    // public resize$ = this.onResize.asObservable();
 
     constructor() { }
 
---
+++ projects/uae-iserve/src/app/features/fleet/test.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `test`
+})
+
+export class TestComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
