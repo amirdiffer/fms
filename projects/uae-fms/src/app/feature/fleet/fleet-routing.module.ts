e3bada6582b6300a8814d58aa560a59a9644d7e7
Moretza Moradi <morteza.moradi@jointscopes.com>
2021-01-25 13:04:53 +0330

    working on accessory

--- projects/uae-fms/src/app/feature/fleet/fleet-routing.module.ts
+++ projects/uae-fms/src/app/feature/fleet/fleet-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { FleetComponent } from './fleet.component';
+import { AccessoryComponent } from '@feature/fleet/accessory/accessory.component';
 
-const routes: Routes = [{ path: '', component: FleetComponent }];
+const routes: Routes = [
+  { path: '', component: FleetComponent },
+  { path: 'accessory', component: AccessoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/uae-fms/src/app/feature/fleet/fleet.module.ts
+++ projects/uae-fms/src/app/feature/fleet/fleet.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { FleetRoutingModule } from './fleet-routing.module';
 import { FleetComponent } from './fleet.component';
+import { AccessoryComponent } from './accessory/accessory.component';
+import { FilterModule } from '@core/filter/filter.module';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { TableModule } from '@core/table';
 
 @NgModule({
-  declarations: [FleetComponent],
-  imports: [CommonModule, FleetRoutingModule]
+  declarations: [FleetComponent, AccessoryComponent],
+  imports: [
+    CommonModule,
+    FleetRoutingModule,
+    FilterModule,
+    AngularSvgIconModule,
+    TableModule
+  ]
 })
 export class FleetModule {}
