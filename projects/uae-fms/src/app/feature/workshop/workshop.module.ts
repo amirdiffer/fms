e21ab1f7b6903fe4e6580181af12f7c288f4fe39
alireza <ali.hamidi@jointscopes.com>
2021-01-26 15:58:28 +0330

    Bodyshop,Table Improvments

--- projects/uae-fms/src/app/feature/workshop/workshop.module.ts
+++ projects/uae-fms/src/app/feature/workshop/workshop.module.ts
@@ -3,9 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { WorkshopRoutingModule } from './workshop-routing.module';
 import { WorkshopComponent } from './workshop.component';
+import { BodyShopComponent } from './body-shop/body-shop.component';
+
+import { TableModule } from "@core/table";
+import { FilterModule } from "@core/filter";
+import { TabViewModule } from "@core/tab-view";
 
 @NgModule({
-  declarations: [WorkshopComponent],
-  imports: [CommonModule, WorkshopRoutingModule]
+  declarations: [
+    WorkshopComponent,
+    BodyShopComponent
+  ],
+  imports: [
+    TableModule,
+    CommonModule,
+    FilterModule,
+    TabViewModule,
+    WorkshopRoutingModule
+  ]
 })
-export class WorkshopModule {}
+export class WorkshopModule { }
