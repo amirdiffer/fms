60c44b7a01f19401cfe627f37d7c1bc95f08171a
Hamid Mottaghian <mottaghian.hamid@gmail.com>
2021-01-26 16:38:33 +0330

    task master component created

--- projects/uae-fms/src/app/feature/workshop/workshop-routing.module.ts
+++ projects/uae-fms/src/app/feature/workshop/workshop-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskMasterComponent } from './task-master/task-master.component';
 
 import { WorkshopComponent } from './workshop.component';
 
-const routes: Routes = [{ path: '', component: WorkshopComponent }];
+const routes: Routes = [
+  { path: '', component: WorkshopComponent },
+  { path: 'task-master', component: TaskMasterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/uae-fms/src/app/feature/workshop/workshop.module.ts
+++ projects/uae-fms/src/app/feature/workshop/workshop.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { WorkshopRoutingModule } from './workshop-routing.module';
 import { WorkshopComponent } from './workshop.component';
-
+import { TaskMasterComponent } from './task-master/task-master.component';
+import { TableModule } from '@core/table/table.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
-  declarations: [WorkshopComponent],
-  imports: [CommonModule, WorkshopRoutingModule]
+  declarations: [WorkshopComponent, TaskMasterComponent],
+  imports: [CommonModule, WorkshopRoutingModule, TableModule,FontAwesomeModule]
 })
 export class WorkshopModule {}
