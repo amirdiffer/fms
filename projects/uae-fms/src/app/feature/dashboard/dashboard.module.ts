9bc0b5e1d9f9aa5a4ec1433009ed9916948f6cfd
Hamid Mottaghian <mottaghian.hamid@gmail.com>
2021-01-26 12:37:29 +0330

    pull repo and use table component - svg fixed - lazy loading fixed

---
+++ projects/uae-fms/src/app/feature/dashboard/dashboard.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Angular Material
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatTableModule} from '@angular/material/table';
+
+
+// Component
+import { DashboardComponent } from './dashboard.component';
+import { AssetsStatusComponent } from './assets-status/assets-status.component';
+import { ActiveAssetsComponent } from './active-assets/active-assets.component';
+import { SuppliersComponent } from './suppliers/suppliers.component';
+import { BusinessCategoryComponent } from './business-category/business-category.component';
+import { FactoryCategoryComponent } from './factory-category/factory-category.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { TableModule } from '@core/table/table.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent, 
+    AssetsStatusComponent, 
+    ActiveAssetsComponent, 
+    SuppliersComponent, 
+    BusinessCategoryComponent, 
+    FactoryCategoryComponent,
+    ProgressBarComponent
+  ],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    DashboardRoutingModule,
+    MatProgressBarModule,
+    MatTableModule,
+    NgApexchartsModule,
+    TableModule
+
+  ]
+})
+export class DashboardModule { }
