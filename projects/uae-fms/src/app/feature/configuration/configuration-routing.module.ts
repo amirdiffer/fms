2b91d62060782934e199cc53783ba41406bc251e
Moretza Moradi <morteza.moradi@jointscopes.com>
2021-01-27 16:40:39 +0330

    working on business Category

--- projects/uae-fms/src/app/feature/configuration/configuration-routing.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ConfigurationComponent } from './configuration.component';
+import { BusinessCategoryComponent } from '@feature/configuration/business-category/business-category.component';
 
-const routes: Routes = [{ path: '', component: ConfigurationComponent }];
+const routes: Routes = [
+  { path: '', component: ConfigurationComponent },
+  { path: 'business-category', component: BusinessCategoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/uae-fms/src/app/feature/configuration/configuration.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ConfigurationRoutingModule } from './configuration-routing.module';
 import { ConfigurationComponent } from './configuration.component';
+import { BusinessCategoryComponent } from './business-category/business-category.component';
+import { TableModule } from '@core/table';
 
 @NgModule({
-  declarations: [ConfigurationComponent],
-  imports: [CommonModule, ConfigurationRoutingModule]
+  declarations: [ConfigurationComponent, BusinessCategoryComponent],
+  imports: [CommonModule, ConfigurationRoutingModule, TableModule]
 })
 export class ConfigurationModule {}
