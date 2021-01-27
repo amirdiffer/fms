e753623ef947fbfaf33ff9a4cde4508a01234c21
alireza <ali.hamidi@jointscopes.com>
2021-01-27 10:47:47 +0330

    Inception

--- projects/uae-fms/src/app/feature/workshop/workshop.module.ts
+++ projects/uae-fms/src/app/feature/workshop/workshop.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { WorkshopRoutingModule } from './workshop-routing.module';
-import { WorkshopComponent } from './workshop.component';
-import { BodyShopComponent } from './body-shop/body-shop.component';
-
 import { TableModule } from "@core/table";
 import { FilterModule } from "@core/filter";
 import { TabViewModule } from "@core/tab-view";
 
+import { WorkshopRoutingModule } from './workshop-routing.module';
+
+import { WorkshopComponent } from './workshop.component';
+import { BodyShopComponent } from './body-shop/body-shop.component';
+import { AuctionListComponent } from "./inspections/auction-list/auction-list.component";
+import { TechnicalInspectionComponent } from "./inspections/technical-inspection/technical-inspection.component";
+
+
 @NgModule({
   declarations: [
     WorkshopComponent,
-    BodyShopComponent
+    BodyShopComponent,
+    AuctionListComponent,
+    TechnicalInspectionComponent
   ],
   imports: [
     TableModule,
