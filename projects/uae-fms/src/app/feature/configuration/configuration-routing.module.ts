7928d57df095e7df3e4a2c0bd792e9bac64b4e88
Hamid Mottaghian <mottaghian.hamid@gmail.com>
2021-01-27 17:36:35 +0330

    asset configuration ui complele

--- projects/uae-fms/src/app/feature/configuration/configuration-routing.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AssetConfigurationComponent } from './asset-configuration/asset-configuration.component';
 
 import { ConfigurationComponent } from './configuration.component';
 
-const routes: Routes = [{ path: '', component: ConfigurationComponent }];
+const routes: Routes = [
+  { path: '', component: ConfigurationComponent },
+  { path: 'asset-configuration', component: AssetConfigurationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/uae-fms/src/app/feature/configuration/configuration.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration.module.ts
@@ -3,9 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { ConfigurationRoutingModule } from './configuration-routing.module';
 import { ConfigurationComponent } from './configuration.component';
+import { AssetConfigurationComponent } from './asset-configuration/asset-configuration.component';
+import { AssetCategoryComponent } from './asset-configuration/asset-category/asset-category.component';
+import { AssetTypeComponent } from './asset-configuration/asset-type/asset-type.component';
+import { FilterModule } from '@core/filter';
+import { MatCardModule } from '@angular/material/card';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { TableModule } from '@core/table';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
-  declarations: [ConfigurationComponent],
-  imports: [CommonModule, ConfigurationRoutingModule]
+  declarations: [
+    ConfigurationComponent, 
+    AssetConfigurationComponent, 
+    AssetCategoryComponent, 
+    AssetTypeComponent],
+  imports: [
+    CommonModule, 
+    ConfigurationRoutingModule,
+    FilterModule,
+    MatCardModule,
+    AngularSvgIconModule,
+    TableModule,
+    MatRadioModule
+  ]
 })
 export class ConfigurationModule {}
