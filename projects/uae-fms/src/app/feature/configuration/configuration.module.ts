fa9dbaf0b4b26b3ae58d2a006f93e7ac39ddc0c6
alireza <ali.hamidi@jointscopes.com>
2021-01-27 22:57:54 +0330

    Some Fixes

--- projects/uae-fms/src/app/feature/configuration/configuration.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration.module.ts
@@ -4,9 +4,17 @@ import { CommonModule } from '@angular/common';
 import { ConfigurationRoutingModule } from './configuration-routing.module';
 import { ConfigurationComponent } from './configuration.component';
 import { PeriodicServiceComponent } from '@feature/configuration/periodic-service/periodic-service.component';
+import { TableModule } from "@core/table";
 
 @NgModule({
-  declarations: [ConfigurationComponent, PeriodicServiceComponent],
-  imports: [CommonModule, ConfigurationRoutingModule]
+  declarations: [
+    ConfigurationComponent,
+    PeriodicServiceComponent
+  ],
+  imports: [
+    CommonModule,
+    ConfigurationRoutingModule,
+    TableModule
+  ]
 })
-export class ConfigurationModule {}
+export class ConfigurationModule { }
