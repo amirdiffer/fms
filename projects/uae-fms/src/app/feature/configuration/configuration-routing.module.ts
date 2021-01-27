92a81062b4696603f024d68dc6077c463e59092e
Amir Hossein Hosseini <amir.hosseini@jointscopes.com>
2021-01-27 20:43:04 +0330

    added fleet_status to feature/configuration

--- projects/uae-fms/src/app/feature/configuration/configuration-routing.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { ConfigurationComponent } from './configuration.component';
+import { FleetStatusComponent } from '@feature/configuration/fleet-status/fleet-status.component';
 
-const routes: Routes = [{ path: '', component: ConfigurationComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: 'fleet-status', component: ConfigurationComponent },
+  { path: 'fleet-status', component: FleetStatusComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/uae-fms/src/app/feature/configuration/configuration.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration.module.ts
@@ -3,9 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { ConfigurationRoutingModule } from './configuration-routing.module';
 import { ConfigurationComponent } from './configuration.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TableModule } from '@core/table';
+import { FleetStatusComponent } from './fleet-status/fleet-status.component';
+import { TabViewModule } from '@core/tab-view';
+import { FleetStatusAssetTableComponent } from './fleet-status/fleet-status-table/fleet-status-asset-table.component';
 
 @NgModule({
-  declarations: [ConfigurationComponent],
-  imports: [CommonModule, ConfigurationRoutingModule]
+  declarations: [
+    ConfigurationComponent,
+    FleetStatusComponent,
+    FleetStatusAssetTableComponent
+  ],
+  imports: [
+    CommonModule,
+    ConfigurationRoutingModule,
+    FontAwesomeModule,
+    TableModule,
+    TabViewModule
+  ]
 })
 export class ConfigurationModule {}
