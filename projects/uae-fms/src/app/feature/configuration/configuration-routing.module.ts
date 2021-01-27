30166a0cad4e3294a185170f9a7fb6ff8ef57006
Moretza Moradi <morteza.moradi@jointscopes.com>
2021-01-27 16:55:23 +0330

    working on Ownership

--- projects/uae-fms/src/app/feature/configuration/configuration-routing.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ConfigurationComponent } from './configuration.component';
+import { OwnershipComponent } from '@feature/configuration/ownership/ownership.component';
 
-const routes: Routes = [{ path: '', component: ConfigurationComponent }];
+const routes: Routes = [
+  { path: '', component: ConfigurationComponent },
+  { path: 'ownership', component: OwnershipComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- projects/uae-fms/src/app/feature/configuration/configuration.module.ts
+++ projects/uae-fms/src/app/feature/configuration/configuration.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ConfigurationRoutingModule } from './configuration-routing.module';
 import { ConfigurationComponent } from './configuration.component';
+import { OwnershipComponent } from './ownership/ownership.component';
+import { TableModule } from '@core/table';
 
 @NgModule({
-  declarations: [ConfigurationComponent],
-  imports: [CommonModule, ConfigurationRoutingModule]
+  declarations: [ConfigurationComponent, OwnershipComponent],
+  imports: [CommonModule, ConfigurationRoutingModule, TableModule]
 })
 export class ConfigurationModule {}
