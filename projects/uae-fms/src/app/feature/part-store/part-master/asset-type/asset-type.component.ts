e496260f1e4cae9f298903f9c0a865e6c15b59bf
Hamid Mottaghian <mottaghian.hamid@gmail.com>
2021-01-28 09:47:44 +0330

    fix errors

---
+++ projects/uae-fms/src/app/feature/part-store/part-master/asset-type/asset-type.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'part-store-asset-type',
+  templateUrl: './asset-type.component.html',
+  styleUrls: ['./asset-type.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AssetTypeComponent implements OnInit {
+  defaultSelecAssetType ="car";
+  defaultSelectMake= "bmw";
+  defaultSelectModel = "m6"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
