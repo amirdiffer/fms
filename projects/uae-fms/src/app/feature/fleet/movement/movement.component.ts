d1fd809e8f3e637c79da7ccd4485ef5c178b3567
Hamid Mottaghian <mottaghian.hamid@gmail.com>
2021-01-30 12:28:25 +0330

    fix conflict

--- projects/uae-fms/src/app/feature/fleet/movement/movement.component.ts
+++ projects/uae-fms/src/app/feature/fleet/movement/movement.component.ts
@@ -8,7 +8,6 @@ import { Observable, of } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MovementComponent implements OnInit, AfterViewChecked {
-  searchIcon= 'assets/icons/search.svg'
   filterSetting;
   movementOverViewTableSetting;
   requestTableSetting;
