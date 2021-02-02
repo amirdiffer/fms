80f124c7ec0a3bdc5e558d42c88926a74d96cea4
Moretza Moradi <morteza.moradi@jointscopes.com>
2021-02-02 10:48:49 +0330

    working on organization_form

--- projects/uae-fms/src/app/feature/fleet/fleet.module.ts
+++ projects/uae-fms/src/app/feature/fleet/fleet.module.ts
@@ -13,6 +13,7 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 import { SubAssetComponent } from './sub-asset/sub-asset.component';
 import { OrganizationComponent } from './organization/organization.component';
 import { MovementComponent } from './movement/movement.component';
+import { AddOrganizationComponent } from './organization/add-organization/add-organization.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { MovementComponent } from './movement/movement.component';
     SubAssetComponent,
     AssetsComponent,
     AccessoryComponent,
-    MovementComponent
+    MovementComponent,
+    AddOrganizationComponent
   ],
   imports: [
     CommonModule,
