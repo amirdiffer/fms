b44f5e7746eaf462c87d890a9b9cd72ba4bc58c6
Moretza Moradi <morteza.moradi@jointscopes.com>
2021-02-06 14:23:16 +0330

    working on fuell_management_forms

--- projects/uae-fms/src/app/feature/fuel-management/fuel-management.module.ts
+++ projects/uae-fms/src/app/feature/fuel-management/fuel-management.module.ts
@@ -6,14 +6,31 @@ import { TabViewModule } from '@core/tab-view';
 import { FuelRoutingModule } from '@feature/fuel-management/fuel-routing.module';
 import { FuelCardTableComponent } from '@feature/fuel-management/fuel-card-table/fuel-card-table.component';
 import { AssetUsageTableComponent } from '@feature/fuel-management/asset-usage-table/asset-usage-table.component';
+import { AddFuelCardComponent } from './add-fuel-card/add-fuel-card.component';
+import { TableModule } from '@core/table/table.module';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
   declarations: [
     FuelManagementComponent,
     FuelCardTableComponent,
-    AssetUsageTableComponent
+    AssetUsageTableComponent,
+    AddFuelCardComponent
   ],
-  imports: [CommonModule, FuelRoutingModule, FilterModule, TabViewModule],
+  imports: [
+    CommonModule,
+    FuelRoutingModule,
+    FilterModule,
+    TabViewModule,
+    TableModule,
+    AutoCompleteModule,
+    DropdownModule,
+    InputTextModule,
+    CalendarModule
+  ],
   exports: [FuelManagementComponent]
 })
 export class FuelManagementModule {}
