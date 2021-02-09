003a5188eabbbf4004a4b3839d4f9211550061b7
Moretza Moradi <morteza.moradi@jointscopes.com>
2021-02-09 13:35:09 +0330

    add entity models

---
+++ projects/uae-fms/src/app/models/organization.ts
@@ -0,0 +1,11 @@
+export interface IOrganization {
+  organizationNumber: 19821240;
+  organizationName: 11520067;
+  tags: string[];
+  departments: IDepartment[];
+}
+
+export interface IDepartment {
+  name: string;
+  locationAddress: string;
+}
