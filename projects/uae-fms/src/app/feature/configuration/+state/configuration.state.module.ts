d7441ee5baa3e4ab70b5408dab16fd1588e0a811
alireza <ali.hamidi@jointscopes.com>
2021-02-12 18:55:25 +0330

    Some refactors

--- projects/uae-fms/src/app/feature/configuration/+state/configuration.state.module.ts
+++ projects/uae-fms/src/app/feature/configuration/+state/configuration.state.module.ts
@@ -1,32 +1,25 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
-import { CONFIGURATION_FEATURE_KEY } from './configuration.entity';
-import { reducers } from './configuration.reducer';
 import { EffectsModule } from '@ngrx/effects';
+
+import { UsersEffect } from './users/users.effect';
+import { reducers } from './configuration.reducer';
+import { UsersFacade, UsersService } from './users';
+import { OwnershipFacade, OwnershipService } from './ownership';
+import { OwnershipEffect } from './ownership/ownership.effect';
+import { CONFIGURATION_FEATURE_KEY } from './configuration.entity';
+import { AssetPolicyEffect } from './asset-policy/asset-policy.effect';
+import { AssetPolicyFacade, AssetPolicyService } from './asset-policy';
+import { FleetStatusFacade, FleetStatusService } from './fleet-status';
+import { FleetStatusEffect } from './fleet-status/fleet-status.effect';
 import { RolePermissionEffect } from './role-permission/role-permission.effect';
 import { RolePermissionFacade } from './role-permission/role-permission.facade';
 import { RolePermissionService } from './role-permission/role-permission.service';
-import { AssetPolicyEffect } from './asset-policy/asset-policy.effect';
-import { AssetPolicyFacade, AssetPolicyService } from './asset-policy';
-import { UsersFacade, UsersService } from './users';
-import { UsersEffect } from './users/users.effect';
-<<<<<<< HEAD
-import {
-  BusinessCategoryFacade,
-  BusinessCategoryService
-} from './business-category';
 import { BusinessCategoryEffect } from './business-category/business-category.effect';
-=======
-import { FleetStatusEffect } from './fleet-status/fleet-status.effect';
-import { FleetStatusFacade, FleetStatusService } from './fleet-status';
-import { OwnershipFacade, OwnershipService } from './ownership';
-import { OwnershipEffect } from './ownership/ownership.effect';
+import { BusinessCategoryFacade, BusinessCategoryService } from './business-category';
+
+import { PeriodicServiceFacade, PeriodicServiceService } from '../+state/periodic-service';
 import { PeriodicServiceEffect } from '../+state/periodic-service/periodic-service.effect';
-import {
-  PeriodicServiceFacade,
-  PeriodicServiceService
-} from '../+state/periodic-service';
->>>>>>> 44636e5d6b8dc59a2eb4dd46a91ef727e91ae33c
 
 @NgModule({
   declarations: [],
@@ -36,13 +29,10 @@ import {
       RolePermissionEffect,
       AssetPolicyEffect,
       UsersEffect,
-<<<<<<< HEAD
-      BusinessCategoryEffect
-=======
+      BusinessCategoryEffect,
       FleetStatusEffect,
       OwnershipEffect,
       PeriodicServiceEffect
->>>>>>> 44636e5d6b8dc59a2eb4dd46a91ef727e91ae33c
     ])
   ],
   providers: [
@@ -52,17 +42,14 @@ import {
     AssetPolicyService,
     UsersFacade,
     UsersService,
-<<<<<<< HEAD
     BusinessCategoryService,
-    BusinessCategoryFacade
-=======
+    BusinessCategoryFacade,
     FleetStatusFacade,
     FleetStatusService,
     OwnershipFacade,
     OwnershipService,
     PeriodicServiceFacade,
     PeriodicServiceService
->>>>>>> 44636e5d6b8dc59a2eb4dd46a91ef727e91ae33c
   ]
 })
-export class ConfigurationStateModule {}
+export class ConfigurationStateModule { }
