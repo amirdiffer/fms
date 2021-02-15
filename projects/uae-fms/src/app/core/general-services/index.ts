83095ae78f6f6a9c3cfde8ef99196b4f7e958406
alireza <ali.hamidi@jointscopes.com>
2021-02-15 09:43:17 +0330

    Window Resize Service Updated

--- projects/uae-fms/src/app/core/general-services/index.ts
+++ projects/uae-fms/src/app/core/general-services/index.ts
@@ -1,2 +1,2 @@
-export { WindowResizeService } from './window-resize.service';
+export { WindowResizeService, ResizeEvent } from './window-resize.service';
 export { GeneralServicesModule } from './general-services.module';
