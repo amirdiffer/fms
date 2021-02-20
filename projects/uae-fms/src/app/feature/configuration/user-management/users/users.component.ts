e535aedaa33d488f0acf1a07a9ed56e81faa32eb
Morteza Moradi <motazad8556@gmail.com>
2021-02-20 17:09:05 +0330

    add transition userManagement

--- projects/uae-fms/src/app/feature/configuration/user-management/users/users.component.ts
+++ projects/uae-fms/src/app/feature/configuration/user-management/users/users.component.ts
@@ -13,26 +13,26 @@ export class UsersComponent implements OnInit {
   downloadBtn = 'assets/icons/download-solid.svg';
   filterCard: FilterCardSetting[] = [
     {
-      filterTitle: 'This Month',
+      filterTitle: 'statistic.this_month',
       filterCount: '',
       filterTagColor: '',
       isCalendar: true,
       onActive(index: number) {}
     },
     {
-      filterTitle: 'Total',
+      filterTitle: 'statistic.total',
       filterCount: '356',
       filterTagColor: '#6EBFB5',
       onActive(index: number) {}
     },
     {
-      filterTitle: 'Active',
+      filterTitle: 'statistic.active',
       filterCount: '124',
       filterTagColor: '#6870B4',
       onActive(index: number) {}
     },
     {
-      filterTitle: 'Inactive',
+      filterTitle: 'statistic.inactive',
       filterCount: '12',
       filterTagColor: '#BA7967',
       onActive(index: number) {}
@@ -42,26 +42,26 @@ export class UsersComponent implements OnInit {
   users_Table: TableSetting = {
     columns: [
       {
-        lable: 'User',
+        lable: 'tables.column.user',
         type: 1,
         field: 'User',
         renderer: 'userRenderer',
         thumbField: 'picture'
       },
       {
-        lable: 'Department',
+        lable: 'tables.column.department',
         type: 1,
         field: 'Department',
         renderer: 'doubleLineRenderer'
       },
       {
-        lable: 'Information',
+        lable: 'tables.column.information',
         type: 1,
         field: 'Information',
         renderer: 'doubleLineRenderer'
       },
-      { lable: 'Status', type: 1, field: 'Status' },
-      { lable: 'Role', type: 1, field: 'Role' }
+      { lable: 'tables.column.status', type: 1, field: 'Status' },
+      { lable: 'tables.column.role', type: 1, field: 'Role' }
     ],
     data: [
       {
