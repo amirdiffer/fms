14ad8c81067059abed462c90e8bc1e927d852c79
Morteza Moradi <motazad8556@gmail.com>
2021-02-27 15:55:55 +0330

    fix state

--- projects/uae-fms/src/app/feature/configuration/+state/periodic-service/periodic-service.service.ts
+++ projects/uae-fms/src/app/feature/configuration/+state/periodic-service/periodic-service.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
 import { IPeriodicService } from '@models/configuration';
+import { ResponseBody } from '@models/response-body';
 
 @Injectable()
 export class PeriodicServiceService {
   constructor(private http: HttpClient) {}
 
-  loadAll(): Observable<IPeriodicService[]> {
-    return this.http.get<IPeriodicService[]>(
+  loadAll(): Observable<ResponseBody<IPeriodicService[]>> {
+    return this.http.get<ResponseBody<IPeriodicService[]>>(
       environment.baseApiUrl + 'configuration/periodic-service'
     );
   }
