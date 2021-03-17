54b0dc81f7e0183994afda4cf12ddebc1ccca854
Amir Hossein Hosseini <amir.hosseini@jointscopes.com>
2021-03-17 22:07:25 +0330

    Fixed problem according to new subtasks

--- projects/uae-fms/src/app/feature/configuration/user-management/users/add-user/add-user.component.ts
+++ projects/uae-fms/src/app/feature/configuration/user-management/users/add-user/add-user.component.ts
@@ -1,12 +1,13 @@
 import { InjectableCompiler } from '@angular/compiler/src/injectable_compiler';
 import {
+  AfterContentInit,
   ChangeDetectionStrategy,
   Component,
   Injector,
   OnDestroy,
   OnInit
 } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FilterCardSetting } from '@core/filter';
 import { Utility } from '@shared/utility/utility';
 import {
@@ -23,7 +24,9 @@ import { DataService } from '../data.service';
   styleUrls: ['./add-user.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AddUserComponent extends Utility implements OnInit, OnDestroy {
+export class AddUserComponent
+  extends Utility
+  implements OnInit, AfterContentInit, OnDestroy {
   formChangesSubscription!: Subscription;
 
   dialogSetting: IDialogAlert = {
@@ -95,6 +98,18 @@ export class AddUserComponent extends Utility implements OnInit, OnDestroy {
     }
   ];
 
+  tempImage: ArrayBuffer | string = '';
+
+  get emails(): FormArray {
+    return this.form.get('personalInformation').get('emails') as FormArray;
+  }
+
+  get phoneNumbers(): FormArray {
+    return this.form
+      .get('personalInformation')
+      .get('phoneNumbers') as FormArray;
+  }
+
   constructor(
     injector: Injector,
     private formBuilder: FormBuilder,
@@ -126,75 +141,91 @@ export class AddUserComponent extends Utility implements OnInit, OnDestroy {
 
       this.form.controls['personalInformation'].patchValue({
         firstName: this.dataService.dataToEditFromTable.firstName,
-        lastName: this.dataService.dataToEditFromTable.lastName,
-        email: this.dataService.dataToEditFromTable.Information.line1,
+        lastName: this.dataService.dataToEditFromTable.lastName
+      });
+
+      this.emails.controls[0].patchValue({
+        email: this.dataService.dataToEditFromTable.Information.line1
+      });
+
+      this.phoneNumbers.controls[0].patchValue({
         phoneNumber: this.dataService.dataToEditFromTable.Information.line2
       });
 
       this.form.controls['fileUpload'].patchValue({
         fileName: this.dataService.dataToEditFromTable.picture
       });
-    }
 
-    /*
-    Department:
-    line1: "Department name"
-    line2: "Section Name"
-    __proto__: Object
-    Information:
-    line1: "sample@gmail.com"
-    line2: "+97150563793"
-    __proto__: Object
-    Role: "Fleet Manager"
-    Status: "Active"
-    firstName: "Sam"
-    id: "1234567899"
-    lastName: "Smith"
-    picture: "user-image.png"
-    statusColor: "#7F87CA"
-     */
-
-    this.formChangesSubscription = this.form.valueChanges.subscribe(
-      (formValues) => {
-        if (formValues.portalInformation.employeeNumber.name) {
-          this.form.controls['personalInformation'].patchValue(
-            {
-              firstName: formValues.portalInformation.employeeNumber.name,
-              lastName: formValues.portalInformation.employeeNumber.name
-            },
-            { emitEvent: false }
-          );
+      this.formChangesSubscription = this.form.valueChanges.subscribe(
+        (formValues) => {
+          if (formValues.portalInformation.employeeNumber.name) {
+            this.form.controls['personalInformation'].patchValue(
+              {
+                firstName: formValues.portalInformation.employeeNumber.name,
+                lastName: formValues.portalInformation.employeeNumber.name
+              },
+              { emitEvent: false }
+            );
+          }
         }
-      }
-    );
+      );
+    }
+  }
+
+  ngAfterContentInit(): void {
+    this.formChangesSubscription?.unsubscribe();
   }
 
   buildForm(): void {
     this.form = this.formBuilder.group({
       portalInformation: this.formBuilder.group({
         employeeNumber: ['', [Validators.required]],
-        department: [''],
+        department: ['', [Validators.required]],
         role: [''],
         activeEmployee: false
       }),
       fileUpload: this.formBuilder.group({
         fileName: 'file.pdf',
-        fileSize: '00 MB'
+        fileSize: '00 MB',
+        file: [undefined, [Validators.required]]
       }),
       personalInformation: this.formBuilder.group({
         firstName: ['', [Validators.required]],
         lastName: ['', [Validators.required]],
-        email: ['', [Validators.required, Validators.email]],
-        phoneNumber: [
-          '',
-          [Validators.required, Validators.pattern('^(00|\\+|)[0-9]{10,12}')]
-        ],
+        emails: new FormArray([this.createEmailField()]),
+        phoneNumbers: new FormArray([this.createPhoneField()]),
         callCheckbox: false,
         smsCheckbox: false,
         emailCheckbox: false,
         whatsappCheckbox: false
       })
     });
+  }
+
+  createEmailField(): FormGroup {
+    return this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  addEmailField(): void {
+    if (this.form.get('personalInformation').get('emails').invalid) {
+      return;
+    }
+    this.emails.push(this.createEmailField());
+  }
+
+  createPhoneField(): FormGroup {
+    return this.formBuilder.group({
+      phoneNumber: ['', [Validators.pattern('^(00|\\+|)[0-9]{10,12}')]]
+    });
+  }
+
+  addPhoneField(): void {
+    if (this.form.get('personalInformation').get('phoneNumbers').invalid) {
+      return;
+    }
+    this.phoneNumbers.push(this.createPhoneField());
   }
 
   dialogConfirm($event): void {
@@ -208,30 +239,43 @@ export class AddUserComponent extends Utility implements OnInit, OnDestroy {
 
   cancel(): void {
     this.dialogModal = true;
-    this.dialogSetting.hasError = false;
-    this.dialogSetting.message =
-      'Are you sure that you want to cancel adding new user?';
-    this.dialogSetting.confirmButton = 'Yes';
-  }
-
-  submit(): void {
-    this.dialogModal = true;
-    this.submited = true;
-    if (this.form.invalid) {
-      this.dialogSetting.hasError = true;
-      this.dialogSetting.message = 'Some fields are empty, please fill them.';
-      this.dialogSetting.confirmButton = 'OK';
-      this.dialogSetting.cancelButton = undefined;
+    if (this.dataService.isEditing) {
+      this.dialogSetting.header = 'Edit user';
+      this.dialogSetting.hasError = false;
+      this.dialogSetting.isWarning = true;
+      this.dialogSetting.message =
+        'Are you sure that you want to cancel editing user?';
+      this.dialogSetting.confirmButton = 'Yes';
+      this.dialogSetting.cancelButton = 'Cancel';
       return;
     }
 
+    this.dialogSetting.header = 'Add new user';
+    this.dialogSetting.hasError = false;
+    this.dialogSetting.isWarning = true;
+    this.dialogSetting.message =
+      'Are you sure that you want to cancel adding new user?';
+    this.dialogSetting.confirmButton = 'Yes';
+    this.dialogSetting.cancelButton = 'Cancel';
+  }
+
+  submit(): void {
+    this.submited = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.dialogModal = true;
     if (this.dataService.isEditing) {
+      this.dialogSetting.header = 'Add new user';
       this.dialogSetting.message = 'User edited successfully.';
       this.dialogSetting.confirmButton = 'OK';
       this.dialogSetting.cancelButton = undefined;
       return;
     }
 
+    this.dialogSetting.header = 'Add new user';
+    this.dialogSetting.isWarning = false;
     this.dialogSetting.hasError = false;
     this.dialogSetting.message = 'New user added successfully.';
     this.dialogSetting.confirmButton = 'OK';
@@ -260,12 +304,23 @@ export class AddUserComponent extends Utility implements OnInit, OnDestroy {
     ];
   }
   public dropped(files: NgxFileDropEntry[]) {
+    console.log(this.form.value);
     this.filesUpdloaded = files;
     for (const droppedFile of files) {
       if (droppedFile.fileEntry.isFile) {
         const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        const reader = new FileReader();
         fileEntry.file((file: File) => {
-          console.log(droppedFile.relativePath, file);
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+            this.tempImage = reader.result;
+            this.form.controls['fileUpload'].patchValue({
+              fileName: file.name,
+              fileSize: file.size,
+              file: reader.result
+            });
+            console.log(droppedFile.relativePath, file);
+          };
         });
       } else {
         const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
