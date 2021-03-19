f65ccbf6d92cfc7adc315540bcb17cb7729834a7
DifferOfEveryOne <differofeveryone@gmail.com>
2021-03-20 01:47:27 +0330

    All 9 subtasks are done

--- projects/uae-fms/src/app/feature/part-store/order-list/order/order.component.ts
+++ projects/uae-fms/src/app/feature/part-store/order-list/order/order.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Injector,
+  OnInit
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Utility } from '@shared/utility/utility';
+import { IDialogAlert } from '@core/alret-dialog/alret-dialog.component';
 
 @Component({
   selector: 'order-form',
@@ -7,7 +14,19 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./order.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OrderFormComponent implements OnInit {
+export class OrderFormComponent extends Utility implements OnInit {
+  submited = false;
+
+  dialogModal = false;
+
+  dialogSetting: IDialogAlert = {
+    header: 'Add Order',
+    hasError: false,
+    message: 'Message is Here',
+    confirmButton: 'Register Now',
+    cancelButton: 'Cancel'
+  };
+
   checked = false;
   assetTypeOptions = [
     { name: 'Type 1', value: 'Type1' },
@@ -20,19 +39,21 @@ export class OrderFormComponent implements OnInit {
   tableSetting;
   tableData;
   public inputForm: FormGroup;
-  constructor(private _fb: FormBuilder) {}
+  constructor(private _fb: FormBuilder, private injector: Injector) {
+    super(injector);
+  }
 
   ngOnInit(): void {
     this.inputForm = this._fb.group({
       createQuotation: ['repeat'],
       assetType: this._fb.group({
-        type: ['Type1'],
-        make: ['Type1'],
-        model: ['Type1']
+        type: ['Type1', [Validators.required]],
+        make: ['Type1', [Validators.required]],
+        model: ['Type1', [Validators.required]]
       }),
       repeatQuotation: this._fb.group({
-        current: ['Type1'],
-        quantity: [''],
+        current: ['Type1', [Validators.required]],
+        quantity: ['', [Validators.required]],
         description: ['']
       }),
       newOrder: this._fb.group({
@@ -50,7 +71,8 @@ export class OrderFormComponent implements OnInit {
       {
         itemName: 'item Name',
         date: '02/02/2020',
-        suppliers: 'Description is here',
+        suppliers: 'BMW',
+        description: 'Description is here',
         quantity: '12',
         cost: '1000 AED',
         total: '1000 AED',
@@ -59,7 +81,8 @@ export class OrderFormComponent implements OnInit {
       {
         itemName: 'item Name',
         date: '02/02/2020',
-        suppliers: 'Description is here',
+        suppliers: 'BMW',
+        description: 'Description is here',
         quantity: '12',
         cost: '1000 AED',
         total: '1000 AED',
@@ -68,7 +91,8 @@ export class OrderFormComponent implements OnInit {
       {
         itemName: 'item Name',
         date: '02/02/2020',
-        suppliers: 'Description is here',
+        suppliers: 'BMW',
+        description: 'Description is here',
         quantity: '12',
         cost: '1000 AED',
         total: '1000 AED',
@@ -102,6 +126,14 @@ export class OrderFormComponent implements OnInit {
           renderer: ''
         },
         {
+          lable: 'tables.column.description',
+          field: 'description',
+          width: 100,
+          type: 1,
+          thumbField: '',
+          renderer: ''
+        },
+        {
           lable: 'tables.column.quantity',
           field: 'quantity',
           width: 100,
@@ -123,6 +155,7 @@ export class OrderFormComponent implements OnInit {
           width: 100,
           type: 1,
           thumbField: '',
+          sortable: true,
           renderer: ''
         },
         {
@@ -136,5 +169,34 @@ export class OrderFormComponent implements OnInit {
       ],
       data: this.tableData
     };
+  }
+
+  cancel(): void {
+    this.dialogModal = true;
+    this.dialogSetting.isWarning = true;
+    this.dialogSetting.cancelButton = 'No';
+    this.dialogSetting.confirmButton = 'Yes';
+    this.dialogSetting.message = 'Are you sure to cancel adding new order?';
+  }
+
+  dialogConfirm(event: any): void {
+    this.dialogModal = false;
+
+    if (!this.dialogSetting.hasError && event) {
+      this.router.navigate(['/part-store/order-list']).then();
+    }
+  }
+
+  submit(): void {
+    this.submited = true;
+    if (this.inputForm.invalid) {
+      return;
+    }
+
+    this.dialogModal = true;
+    this.dialogSetting.message = 'New order added successfully';
+    this.dialogSetting.isWarning = false;
+    this.dialogSetting.confirmButton = 'OK';
+    this.dialogSetting.cancelButton = undefined;
   }
 }
