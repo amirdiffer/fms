c3d78b09678cfa9b003bf3ad7562ab723e5811b6
Mahdi <mahdi.pour@jointscopes.com>
2021-04-01 13:18:12 +0430

    apply state changes

--- projects/uae-fms/src/app/feature/workshop/+state/task-master/task-master.facade.ts
+++ projects/uae-fms/src/app/feature/workshop/+state/task-master/task-master.facade.ts
@@ -6,15 +6,25 @@ import { TaskMasterSelectors } from './task-master.selectors';
 
 @Injectable()
 export class TaskMasterFacade {
-  bodyShop$ = this.store.pipe(select(TaskMasterSelectors.selectAll));
+  taskMasters$ = this.store.pipe(select(TaskMasterSelectors.selectAll));
 
   message$ = this.store.pipe(select(TaskMasterSelectors.message));
 
   error$ = this.store.pipe(select(TaskMasterSelectors.error));
 
+  submitted$ = this.store.pipe(select(TaskMasterSelectors.submitted));
+
   constructor(private store: Store<ITaskMasterPartialState>) {}
 
   loadAll() {
     this.store.dispatch(TaskMasterActions.loadAll());
+  }
+
+  addTaskMaster(data: any) {
+    this.store.dispatch(TaskMasterActions.addTaskMaster({ data }));
+  }
+
+  editTaskMaster(data: any) {
+    this.store.dispatch(TaskMasterActions.editTaskMaster({ data }));
   }
 }
--- projects/uae-fms/src/app/feature/workshop/task-master/task-master.model.ts
+++ projects/uae-fms/src/app/feature/workshop/task-master/task-master.model.ts
@@ -1,4 +1,5 @@
 export interface ITaskMaster {
+  id?: number;
   taskName: string;
   timeEstimate: string;
   instrunction: string;
