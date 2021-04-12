c7c5f38330ae3452e67a4859876a52725ab701c2
DifferOfEveryOne <differofeveryone@gmail.com>
2021-04-12 11:41:25 +0430

    optimized asset-policy calculation.

--- projects/uae-fms/src/app/feature/fleet/assets/add-asset/add-asset.component.ts
+++ projects/uae-fms/src/app/feature/fleet/assets/add-asset/add-asset.component.ts
@@ -631,8 +631,39 @@ export class AddAssetComponent extends Utility implements OnInit, OnDestroy {
     let maxUsageKPHour = this.policyTypeValue.maxUsageKPHour;
     this.reviewPlaneSettingTable2.data = [];
     let newValue = (value * depreciationValue) / 100;
-    for (let index = 0; index < maxUsageYear; index++) {
+
+    let iterator: number;
+
+    if (maxUsageYear === 0) {
+      iterator = maxUsageKPHour;
+      this.reviewPlaneSettingTable2.columns[0].lable = 'tables.column.km';
+    } else {
+      iterator = maxUsageYear;
+      this.reviewPlaneSettingTable2.columns[0].lable = 'tables.column.year';
+    }
+
+    let kmValue = maxUsageKPHour;
+    for (let index = 0; index < iterator; index++) {
+      if (index > 9) {
+        break;
+      }
+
       value = value - newValue;
+
+      if (value < 0) {
+        break;
+      }
+
+      if (maxUsageYear === 0) {
+        kmValue += maxUsageKPHour;
+        const rowData = {
+          year: kmValue,
+          bookValue: Math.round(value)
+        };
+        this.reviewPlaneSettingTable2.data.push(rowData);
+        continue;
+      }
+
       const data = {
         year: index + 1,
         bookValue: Math.round(value)
