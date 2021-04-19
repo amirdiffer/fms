85639c03c1de6d4938b65f16b77eb67daa426680
Arman Azimifar <armanazimifar14@gmail.com>
2021-04-19 17:42:21 +0430

    refactor(asset registration): connect to api

--- projects/uae-fms/src/app/feature/fleet/assets/asset-detail/asset-car-detail.component.ts
+++ projects/uae-fms/src/app/feature/fleet/assets/asset-detail/asset-car-detail.component.ts
@@ -7,29 +7,29 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 })
 export class AssetCarDetailComponent implements OnInit, OnChanges {
   @Input() asset;
-  vehicle = {
-    id: 1,
-    make: 'bmw.png',
-    vehicle: 'Request No 123456',
-    thumb: 'thumb1.png',
-    type: 'bmw',
-    model: 'I3',
-    plateno: '1234',
-    iserve: '04',
-    status: '1',
-    location: 'Al Ghandi Ato Service Ras A Khor',
-    bodyType: 'Text Type',
-    color: 'Text Type',
-    trim: 'Text Type',
-    group: 'Text Type',
-    department: 'Dep Name-Area-Dubai',
-    licensePlate: '123456',
-    operator: 'User Name',
-    salik: 'Assign',
-    warranty: 'Under Warranty',
-    vin_sn: 'JTDKBRFU9J30593O7',
-    year: '2020'
-  };
+  // vehicle = {
+  //   id: 1,
+  //   make: 'bmw.png',
+  //   vehicle: 'Request No 123456',
+  //   thumb: 'thumb1.png',
+  //   type: 'bmw',
+  //   model: 'I3',
+  //   plateno: '1234',
+  //   iserve: '04',
+  //   status: '1',
+  //   location: 'Al Ghandi Ato Service Ras A Khor',
+  //   bodyType: 'Text Type',
+  //   color: 'Text Type',
+  //   trim: 'Text Type',
+  //   group: 'Text Type',
+  //   department: 'Dep Name-Area-Dubai',
+  //   licensePlate: '123456',
+  //   operator: 'User Name',
+  //   salik: 'Assign',
+  //   warranty: 'Under Warranty',
+  //   vin_sn: 'JTDKBRFU9J30593O7',
+  //   year: '2020'
+  // };
   constructor() {}
 
   ngOnInit(): void {
