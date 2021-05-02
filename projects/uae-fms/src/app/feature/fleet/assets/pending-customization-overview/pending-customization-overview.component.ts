48dc404d9760431912e49b972f675a2519da5041
alirezahamidi@hotmail.com <alirezahamidi@hotmail.com>
2021-05-02 04:43:59 +0430

    Pending Registration List and form updated

--- projects/uae-fms/src/app/feature/fleet/assets/pending-customization-overview/pending-customization-overview.component.ts
+++ projects/uae-fms/src/app/feature/fleet/assets/pending-customization-overview/pending-customization-overview.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AnyCnameRecord, AnySrvRecord } from 'dns';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AssetMasterService } from "../../+state/assets/asset-master";
+import { CustomizationService } from "../../+state/assets/customization";
 
 @Component({
   selector: 'anms-pending-customization-overview',
@@ -7,6 +12,7 @@ import { AnyCnameRecord, AnySrvRecord } from 'dns';
   styleUrls: ['./pending-customization-overview.component.scss']
 })
 export class PendingCustomizationOverviewComponent implements OnInit {
+
   tableSetting;
   tableData: any[];
   searchIcon = 'assets/icons/search-solid.svg';
@@ -21,41 +27,25 @@ export class PendingCustomizationOverviewComponent implements OnInit {
   showFirstCameraForm = true;
   showSecondCameraForm = true;
   showThirdCameraForm = true;
-  constructor() {}
+  assetStatus$: Observable<any>;
+  customizationData$: Observable<any>;
+  customizationData;
+  customizationForm: FormGroup;
+  outp = {
+    subAssets: [],
+    accessories: []
+  }
+  assetId: number;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: AssetMasterService,
+    private fb: FormBuilder,
+    private cusService: CustomizationService
+  ) { }
 
   ngOnInit(): void {
-    this.tableData = [
-      {
-        item: 'Camera',
-        type: 'Sub asset',
-        quantity: 4,
-        attachment: ''
-      },
-      {
-        item: 'Gps',
-        type: 'Sub asset',
-        quantity: 4,
-        attachment: ''
-      },
-      {
-        item: 'Sticker',
-        type: 'Accessory',
-        quantity: 4,
-        attachment: ''
-      },
-      {
-        item: 'Camera',
-        type: 'Sub asset',
-        quantity: 4,
-        attachment: ''
-      },
-      {
-        item: 'Camera',
-        type: 'Sub asset',
-        quantity: 4,
-        attachment: ''
-      }
-    ];
     this.tableSetting = {
       columns: [
         {
@@ -88,7 +78,60 @@ export class PendingCustomizationOverviewComponent implements OnInit {
           renderer: ''
         }
       ],
-      data: this.tableData
+      data: []
     };
+
+    this.route.params.subscribe(x => {
+      if (x?.id) {
+        this.assetId = x.id;
+        this.customizationData$ = this.service.getAssetByID(x.id + "/customization").pipe(map(a => {
+          if (a && a.message) {
+            this.customizationData = a.message;
+            this.customizationData.subAssets.forEach(e => {
+              this.outp.subAssets.push({ checked: false, serial: '' });
+            });
+            this.customizationData.accessories.forEach(e => {
+              this.outp.accessories.push({ checked: false, serial: '' });
+            });
+
+            /* this.customizationForm = this.fb.group({
+              subAssets: new FormArray([this.createFields(a.message.subAssets.length)]),
+              accessories: new FormArray([this.createFields(a.message.accessories.length)])
+            }); */
+
+            return [...a.message.subAssets.map(b => {
+              return {
+                item: `${b.subAssetMakeName} ${b.subAssetModelName}`,
+                type: "Sub Asset"
+              };
+            }),
+            ...a.message.accessories.map(b => {
+              return {
+                item: b.accessoryConfigurationName,
+                type: "Accessory"
+              };
+            })];
+          }
+        }));
+        this.assetStatus$ = this.service.getAssetByID(x.id + "/summary/customization").pipe(map(z => {
+          return z;
+        }));
+      }
+    });
+  }
+
+
+
+  createFields(count): FormGroup {
+    let fi = this.fb.group({
+      serialNumbers: []
+    });
+
+    return fi;
+  }
+
+  save() {
+    console.log(this.outp);
+    this.cusService.compelete()
   }
 }
