aef4f150267aa34426361d405acc3ac5c2e600e1
alireza <ali.hamidi@jointscopes.com>
2021-05-07 04:56:21 +0430

    OwnerShip Table style fixed

--- projects/uae-fms/src/app/feature/configuration/ownership/ownership.component.ts
+++ projects/uae-fms/src/app/feature/configuration/ownership/ownership.component.ts
@@ -21,9 +21,10 @@ export class OwnershipComponent implements OnInit {
         lable: 'tables.column.duration',
         type: 1,
         field: 'Duration',
-        sortable: true
+        sortable: true,
+        width: 140
       },
-      { lable: 'tables.column.purpose', type: 1, field: 'Purpose' },
+      { lable: 'tables.column.purpose', type: 1, field: 'Purpose', width: 100 },
       { lable: 'tables.column.owner_email', type: 1, field: 'Owner_Email' },
       {
         lable: 'tables.column.owner_phone_no',
@@ -35,7 +36,7 @@ export class OwnershipComponent implements OnInit {
         type: 1,
         isIconLable: true,
         field: 'car',
-        width: 100,
+        width: 70,
         sortable: true
       },
       {
@@ -91,7 +92,7 @@ export class OwnershipComponent implements OnInit {
     private facade: OwnershipFacade,
     private tableFacade: TableFacade,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.facade.loadAll();
