6c8a329b0e5377c66a9d4cbf95db30bb75da0e66
amir.hosseini@jointscopes.com <Appleipadpro1>
2021-05-31 17:02:47 +0430

    feat: added new dialog to the project

---
+++ projects/uae-fms/src/app/core/dialog/dialog.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DialogTemplateComponent } from './dialog-template.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { SharedModule } from '@shared/shared.module';
+
+@NgModule({
+  declarations: [DialogTemplateComponent],
+  imports: [
+    CommonModule,
+    AngularSvgIconModule.forRoot(),
+    SharedModule
+  ]
+})
+export class DialogModule {
+}
